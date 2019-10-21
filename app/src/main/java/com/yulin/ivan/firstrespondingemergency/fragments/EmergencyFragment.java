3ba73f0b1aebcaf86d4444bca822abc71bb17f02
Ivan Yulin <yulin.i777@gmail.com>
2019-10-22 02:21:01 +0300

    changed ui

--- app/src/main/java/com/yulin/ivan/firstrespondingemergency/fragments/EmergencyFragment.java
+++ app/src/main/java/com/yulin/ivan/firstrespondingemergency/fragments/EmergencyFragment.java
@@ -4,12 +4,20 @@ import android.app.Dialog;
 import android.content.Context;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.DialogFragment;
 
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
+import com.yulin.ivan.firstrespondingemergency.R;
 import com.yulin.ivan.firstrespondingemergency.helpers.MyFragmentManager;
 
 /**
@@ -44,13 +52,12 @@ public abstract class EmergencyFragment extends DialogFragment {
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
+        setStyle(DialogFragment.STYLE_NORMAL, R.style.MyAlertDialogTheme);
         setCancelable(false); //avoid close on back press
-        AlertDialog alertDialog = new AlertDialog.Builder(getActivity()).create();
 
-//        dialogBuilder = new AlertDialog.Builder(getActivity());
+        AlertDialog alertDialog = new AlertDialog.Builder(getActivity(), R.style.MyAlertDialogTheme).create();
 
         if (message != null) {
-//            dialogBuilder.setMessage(message);
             alertDialog.setMessage(message);
         }
 
@@ -61,12 +68,6 @@ public abstract class EmergencyFragment extends DialogFragment {
                         onDismiss(dialog);
                         onYesSelected();
                     });
-
-//            dialogBuilder.setPositiveButton(yesAnswer,
-//                    (dialog, whichButton) -> {
-//                        onDismiss(dialog);
-//                        onYesSelected();
-//                    });
         }
 
         if (noAnswer != null) {
@@ -77,28 +78,39 @@ public abstract class EmergencyFragment extends DialogFragment {
                         onNoSelected();
                     });
 
-//            dialogBuilder.setNegativeButton(noAnswer,
-//                    (dialog, whichButton) -> {
-//                        onCancel(dialog);
-//                        onNoSelected();
-//                    });
         }
 
-//        if (layout != -1) {
-//            LayoutInflater inflater = (LayoutInflater) getActivity().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-//            View v = inflater.inflate(layout, null);
-////            dialogBuilder.setView(v);
-////            alertDialog.setView(v);
-//        }
+        if (layout != -1) {
+            LayoutInflater inflater = (LayoutInflater) getActivity().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            View v = inflater.inflate(layout, null);
+//            dialogBuilder.setView(v);
+            alertDialog.setView(v);
+        }
 
-        alertDialog.setCancelable(false);
         alertDialog.setCanceledOnTouchOutside(false);
         alertDialog.show();
 
         DisplayMetrics metrics = getResources().getDisplayMetrics();
-        int width = metrics.widthPixels;
+//        int width = metrics.widthPixels;
         int height = metrics.heightPixels;
-        alertDialog.getWindow().setLayout((6 * width) / 7, (4 * height) / 5);
+//        alertDialog.getWindow().setLayout((6 * width) / 7, (4 * height) / 5);
+
+        alertDialog.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+        alertDialog.getWindow().getAttributes().height = WindowManager.LayoutParams.WRAP_CONTENT;//setGravity(Gravity.CENTER);
+
+        Button btnPositive = alertDialog.getButton(AlertDialog.BUTTON_POSITIVE);
+        Button btnNegative = alertDialog.getButton(AlertDialog.BUTTON_NEGATIVE);
+
+        LinearLayout.LayoutParams layoutParams = (LinearLayout.LayoutParams) btnPositive.getLayoutParams();
+        layoutParams.weight = 10;
+        btnPositive.setLayoutParams(layoutParams);
+        btnNegative.setLayoutParams(layoutParams);
+
+        TextView textView = alertDialog.findViewById(android.R.id.message);
+        textView.setTextSize(30);
+        textView.setGravity(Gravity.CENTER);
+        ((LinearLayout.LayoutParams) textView.getLayoutParams()).topMargin = height / 2;
+        ((LinearLayout.LayoutParams) textView.getLayoutParams()).bottomMargin = 80;
 
 
         return alertDialog;
